681e88f9b7c31c0d9037b36c011de86f835fa3b1
wassan128 <wassan.taiko.master@gmail.com>
2018-12-06 12:40:21 +0900

    [refactor] functionalize related flags[wip]

--- main.go
+++ main.go
@@ -8,6 +8,31 @@ import (
 	"gopkg.in/russross/blackfriday.v2"
 )
 
+func setHTMLFlags() blackfriday.HTMLFlags {
+	htmlFlags := blackfriday.CommonHTMLFlags
+	htmlFlags |= blackfriday.FootnoteReturnLinks
+	htmlFlags |= blackfriday.SmartypantsAngledQuotes
+	htmlFlags |= blackfriday.SmartypantsQuotesNBSP
+
+	return htmlFlags
+}
+
+func setExtensionFlags() blackfriday.Extensions {
+	extFlags := blackfriday.CommonExtensions
+	extFlags |= blackfriday.Footnotes
+	extFlags |= blackfriday.HeadingIDs
+	extFlags |= blackfriday.Titleblock
+	extFlags |= blackfriday.DefinitionLists
+
+	return extFlags
+}
+
+func createDirForPublish() {
+	if err := os.Mkdir("public", 0777); err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 	md, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
@@ -15,19 +40,14 @@ func main() {
 		return
 	}
 
-	htmlFlags := blackfriday.CommonHTMLFlags
-	htmlFlags |= blackfriday.FootnoteReturnLinks
-	htmlFlags |= blackfriday.SmartypantsAngledQuotes
-	htmlFlags |= blackfriday.SmartypantsQuotesNBSP
+	htmlFlags := setHTMLFlags()
 	renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{Flags: htmlFlags, Title: "", CSS: ""})
 
-	extFlags := blackfriday.CommonExtensions
-	extFlags |= blackfriday.Footnotes
-	extFlags |= blackfriday.HeadingIDs
-	extFlags |= blackfriday.Titleblock
-	extFlags |= blackfriday.DefinitionLists
 
+	extFlags := setExtensionFlags()
 	html := blackfriday.Run(md, blackfriday.WithExtensions(extFlags), blackfriday.WithRenderer(renderer))
+
+	createDirForPublish()
 	fmt.Println(string(html))
 }
 
--- test.md
+++ test.md
@@ -2,4 +2,5 @@
 ## this is test
 * markdown to html
 * practice golang
+[link](https://wassan128.github.io/blog)
 
