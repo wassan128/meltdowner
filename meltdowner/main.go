407c58b1de649101cfb88d9521319c9a3fc5e50d
wassan128 <wassan.taiko.master@gmail.com>
2018-12-11 09:32:06 +0900

    [add] debug server

--- meltdowner/main.go
+++ meltdowner/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/wassan128/meltdowner/meltdowner/build"
 )
 
 func main() {
 	build.Run()
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	fmt.Println("[*] public/ is being served on http://localhost:5000")
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
