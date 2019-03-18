fe9f2cda0032735c97d255917db34f01dc15df67
wassan128 <wassan.taiko.master@gmail.com>
2019-03-18 23:07:55 +0900

    [update] tag title

--- meltdowner/build/build.go
+++ meltdowner/build/build.go
@@ -196,7 +196,7 @@ func generateTagTopPage(renderer *ChromaRenderer, tagMap map[string][]*parser.Po
     for tag, posts := range tagMap {
         tagPath := createTagDir(tag)
 
-        mdTagTop := fmt.Sprintf("<h2 style='color: #ccc'>tag: #%s</h2>", tag)
+        mdTagTop := fmt.Sprintf("<h1 style='color: #ccc'>#%s</h1>", tag)
         mdTagTop += "<ul class='top'>\n"
         for _, post := range posts {
             if post.Header.Public == false {
