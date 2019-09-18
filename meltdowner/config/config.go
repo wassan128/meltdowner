601caeec44d360935aabf0313f0d512530c9e10a
wassan128 <wassan.taiko.master@gmail.com>
2019-09-18 11:52:30 +0900

    Add validation func for config

--- meltdowner/config/config.go
+++ meltdowner/config/config.go
@@ -30,3 +30,30 @@ func GetConfig() Config {
 
     return config
 }
+
+func (c Config) Validate() []string {
+    checks := []struct{
+        bad bool
+        msg string
+    } {
+        { c.Blog.Title == "", "Title must specifiy." },
+        { c.Blog.SubTitle == "", "Subtitle does not specified." },
+        { c.Blog.RootPath == "", "RootPath must specify." },
+        { c.Blog.IconURL== "", "Icon url does not specified." },
+        { c.Blog.Author== "", "Author does not specified." },
+        {
+            c.GitHub.Repo != "" &&
+            (c.GitHub.Id == "" || c.GitHub.Email == ""),
+            "GitHub repository specified but GitHub ID or Email does not specified.",
+        },
+    }
+
+    var msgs []string
+    for _, check := range checks {
+        if check.bad {
+            msgs = append(msgs, check.msg)
+        }
+    }
+
+    return msgs
+}
